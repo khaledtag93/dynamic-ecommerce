39d3a37486a01aac60fa027445110662dbda5319
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-20 19:27:22 +0300

    feat: add product extra cost model

---
+++ app/Models/ProductExtraCostItem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductExtraCostItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'name',
+        'amount',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
