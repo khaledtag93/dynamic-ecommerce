dae5a8764df906ad22d5338dd5dabbb6a93067e3
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-20 21:04:01 +0300

    config: add paymob unified checkout credentials

--- config/services.php
+++ config/services.php
@@ -32,6 +32,8 @@ return [
     ],
    'paymob' => [
     'api_key'        => env('PAYMOB_API_KEY'),
+    'secret_key'     => env('PAYMOB_SECRET_KEY'),
+    'public_key'     => env('PAYMOB_PUBLIC_KEY'),
     'hmac_secret'    => env('PAYMOB_HMAC_SECRET'),
     'integration_id' => env('PAYMOB_INTEGRATION_ID'),
     'iframe_id'      => env('PAYMOB_IFRAME_ID'),
