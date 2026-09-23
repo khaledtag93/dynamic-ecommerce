c7578bfac94bc13958d767760e9bb5ecfc8a527d
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-24 02:18:43 +0300

    chore: remove temporary product save diagnostic

--- app/Http/Livewire/Admin/Product/ProductForm.php
+++ app/Http/Livewire/Admin/Product/ProductForm.php
@@ -1416,10 +1416,6 @@ class ProductForm extends Component
         'errors' => $errors->toArray(),
     ]);
 }catch (Throwable $e) {
-            if (app()->environment('testing')) {
-                throw $e;
-            }
-
             report($e);
 
             $this->logError('ProductForm save failed with throwable', [
