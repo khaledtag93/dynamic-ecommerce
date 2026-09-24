5d4c8e9e07c2140294f7611632e4dd97659d5ae5
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-25 01:10:10 +0300

    feat: link users to employee profiles

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,10 @@ class User extends Authenticatable
         return $this->hasMany(CustomerAddress::class);
     }
 
+    public function employeeProfile()
+    {
+        return $this->hasOne(EmployeeProfile::class);
+    }
 
     public function growthScore()
     {
