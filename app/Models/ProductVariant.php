5cd303fdf8581ea43979b2bd13a407ed9b58ffbf
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-24 17:22:18 +0300

    feat: make variant barcode assignable

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -13,6 +13,7 @@ class ProductVariant extends Model
     protected $fillable = [
         'product_id',
         'sku',
+        'barcode',
         'price',
         'sale_price',
         'cost_price',
