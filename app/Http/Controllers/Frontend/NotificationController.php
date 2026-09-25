235ba6ac9e180d45d9c4781174b8d3168b5f3201
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-25 20:26:08 +0300

    refactor: clean notification controller imports

--- app/Http/Controllers/Frontend/NotificationController.php
+++ app/Http/Controllers/Frontend/NotificationController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\DatabaseNotification;
 
