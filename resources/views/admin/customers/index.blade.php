684da4dab110c54a7b1a1fd835854d5feabdd02b
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-25 04:04:55 +0300

    ui: adopt shared stat cards in customers

--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -16,12 +16,13 @@
         ['label' => __('Customer revenue'), 'value' => 'EGP '.number_format($stats['revenue'], 2), 'copy' => __('Gross order value linked to registered accounts.'), 'icon' => 'mdi-cash-multiple'],
     ] as $card)
         <div class="col-md-6 col-xl">
-            <div class="admin-card admin-stat-card h-100">
-                <span class="admin-stat-icon"><i class="mdi {{ $card['icon'] }}"></i></span>
-                <div class="admin-stat-label">{{ $card['label'] }}</div>
-                <div class="admin-stat-value">{{ $card['value'] }}</div>
-                <div class="text-muted small mt-2">{{ $card['copy'] }}</div>
-            </div>
+            <x-admin.stat-card
+                :label="$card['label']"
+                :value="$card['value']"
+                :icon="$card['icon']"
+                :help="$card['copy']"
+                class="h-100"
+            />
         </div>
     @endforeach
 </div>
