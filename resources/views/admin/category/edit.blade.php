6bd8cc6bc0d9836e489a5780205fd6117e1c1bdb
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-25 04:16:20 +0300

    ui: align category editor header with Admin V2

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -3,18 +3,15 @@
 @section('title', __('Edit Category'))
 
 @section('content')
-    <div class="admin-page-header">
-        <div>
-            <h1 class="admin-page-title">{{ __('Edit Category') }}</h1>
-            <p class="admin-page-description">{{ __('Update category content, image, and search metadata.') }}</p>
-        </div>
+    <x-admin.page-header
+        :kicker="__('Catalog management')"
+        :title="__('Edit Category')"
+        :description="__('Update category content, image, and search metadata.')"
+    >
+        <a href="{{ route('admin.categories.index') }}" class="btn btn-light admin-btn-soft admin-back-btn"><i class="mdi mdi-arrow-left"></i> {{ __('Back to Categories') }}</a>
+    </x-admin.page-header>
 
-        <div class="admin-page-actions">
-            <a href="{{ route('admin.categories.index') }}" class="btn btn-light admin-btn-soft admin-back-btn"><i class="mdi mdi-arrow-left"></i> {{ __('Back to Categories') }}</a>
-        </div>
-    </div>
-
-    <form action="{{ route('admin.categories.update', $category) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('admin.categories.update', $category) }}" method="POST" enctype="multipart/form-data" data-submit-loading>
         @csrf
         @method('PUT')
         @php($submitLabel = __('Update Category'))
