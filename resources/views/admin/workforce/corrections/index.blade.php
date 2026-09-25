d7da6e3bfa2106f60a54b1858b3f8dac1606b7c9
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-25 04:05:24 +0300

    ui: adopt shared stat cards in workforce

--- resources/views/admin/workforce/corrections/index.blade.php
+++ resources/views/admin/workforce/corrections/index.blade.php
@@ -17,12 +17,13 @@
             ['label' => __('Total requests'), 'value' => $stats['total'], 'copy' => __('Full correction audit history.'), 'icon' => 'mdi-file-document-edit-outline'],
         ] as $card)
             <div class="col-md-6 col-xl-3">
-                <div class="admin-card admin-stat-card h-100">
-                    <span class="admin-stat-icon"><i class="mdi {{ $card['icon'] }}"></i></span>
-                    <div class="admin-stat-label">{{ $card['label'] }}</div>
-                    <div class="admin-stat-value">{{ $card['value'] }}</div>
-                    <div class="text-muted small mt-2">{{ $card['copy'] }}</div>
-                </div>
+                <x-admin.stat-card
+                    :label="$card['label']"
+                    :value="$card['value']"
+                    :icon="$card['icon']"
+                    :help="$card['copy']"
+                    class="h-100"
+                />
             </div>
         @endforeach
     </div>
