9d7c787141ffa58b9eed5b1c88cf940fc399f0ae
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-26 01:56:33 +0300

    test: guard payment settings bilingual integrity

--- tests/Feature/SharedShellTranslationIntegrityTest.php
+++ tests/Feature/SharedShellTranslationIntegrityTest.php
@@ -37,6 +37,7 @@ class SharedShellTranslationIntegrityTest extends TestCase
             resource_path('views/admin/workforce/time-clock.blade.php'),
             resource_path('views/admin/settings/branding.blade.php'),
             resource_path('views/admin/settings/content.blade.php'),
+            resource_path('views/admin/settings/payments.blade.php'),
             resource_path('views/admin/settings/shipping/_nav.blade.php'),
             resource_path('views/admin/settings/shipping/methods.blade.php'),
             resource_path('views/admin/settings/shipping/rates.blade.php'),
