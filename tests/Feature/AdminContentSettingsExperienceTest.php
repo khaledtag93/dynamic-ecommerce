64e6ca734d6f34a73282db6b6140400774ded2cd
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-26 01:40:33 +0300

    test: cover bilingual footer trust settings

--- tests/Feature/AdminContentSettingsExperienceTest.php
+++ tests/Feature/AdminContentSettingsExperienceTest.php
@@ -33,6 +33,9 @@ class AdminContentSettingsExperienceTest extends TestCase
         $this->assertStringContainsString('id="footer_show_trust"', $html);
         $this->assertStringContainsString('id="footer_show_whatsapp"', $html);
         $this->assertStringContainsString('id="footer_show_website"', $html);
+        $this->assertStringContainsString('name="footer_trust_1_text_en"', $html);
+        $this->assertStringContainsString('name="footer_trust_1_text_ar"', $html);
+        $this->assertStringContainsString('name="footer_trust_3_text_ar"', $html);
         $this->assertStringNotContainsString('form-check admin-switch-card h-100 d-block', $html);
     }
     public function test_footer_visibility_settings_are_persisted(): void
@@ -54,6 +57,27 @@ class AdminContentSettingsExperienceTest extends TestCase
         $this->assertDatabaseHas('website_settings', ['key' => 'footer_show_experience_note', 'value' => '0']);
         $this->assertDatabaseHas('website_settings', ['key' => 'footer_show_whatsapp', 'value' => '1']);
         $this->assertDatabaseHas('website_settings', ['key' => 'footer_show_website', 'value' => '0']);
+    }
+
+    public function test_footer_trust_copy_is_persisted_bilingually(): void
+    {
+        $admin = $this->createSuperAdmin();
+
+        $this->actingAs($admin)
+            ->put(route('admin.settings.content.update'), [
+                'footer_show_trust' => '1',
+                'footer_trust_1_text_en' => 'Protected payments',
+                'footer_trust_1_text_ar' => 'مدفوعات محمية',
+                'footer_trust_2_text_en' => 'Helpful service',
+                'footer_trust_2_text_ar' => 'خدمة مفيدة',
+                'footer_trust_3_text_en' => 'Clear information',
+                'footer_trust_3_text_ar' => 'معلومات واضحة',
+            ])
+            ->assertRedirect();
+
+        $this->assertDatabaseHas('website_settings', ['key' => 'footer_trust_1_text_en', 'value' => 'Protected payments']);
+        $this->assertDatabaseHas('website_settings', ['key' => 'footer_trust_1_text_ar', 'value' => 'مدفوعات محمية']);
+        $this->assertDatabaseHas('website_settings', ['key' => 'footer_trust_3_text_en', 'value' => 'Clear information']);
     }
 
 }
