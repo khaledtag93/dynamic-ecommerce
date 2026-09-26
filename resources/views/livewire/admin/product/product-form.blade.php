3fcc1fbf4a5446e1819649df8970fe7d5b2e16ca
khaledtag93 <42767132+khaledtag93@users.noreply.github.com>
2026-09-26 05:06:29 +0300

    refactor: update Livewire initialization hooks for v3

--- resources/views/livewire/admin/product/product-form.blade.php
+++ resources/views/livewire/admin/product/product-form.blade.php
@@ -1698,7 +1698,7 @@
                 initSortableGrid('#newImagesGrid', '.new-image-item', 'reorderNewImages');
             }
 
-            document.addEventListener('livewire:load', () => {
+            document.addEventListener('livewire:init', () => {
                 initProductFormHelpers();
 
                 window.addEventListener('product-form-toast', (event) => {
